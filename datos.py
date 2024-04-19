5e0160864dd12a1c9fe630c6991c77bef6a2c034
Giuliana Cubilla <giulii024.4@gmail.com>
2024-04-19 20:10:11 -0300

    se realizaron pequeños cambios y se agregaron comentarios explicando como funciona cada parte del código

--- datos.py
+++ datos.py
@@ -1,16 +1,30 @@
+#importación de la librería pandas
 import pandas as pd
 
+#Definición de la función llamada "analisis_estadistico" y toma una lista de Edades
 def analisis_estadistico(valores):
+    
+    #Se verifica si la lista está vacía o si no es una lista
     if not valores or not isinstance(valores, list):
+        
+        #Si no se cumple con esa condición, devuelve un error de ValueError
         raise ValueError("La lista no puede estar vacía")
 
-    df = pd.DataFrame(valores, columns=["Valor"])
+    #Se crea un Dataframe
+    df = pd.DataFrame(valores, columns=["Edad"])
 
-    df['fi'] = df['Valor'].map(df['Valor'].value_counts())
+    #Se calcula la frecuencia absoluta de cada valor de la columna "Edad"
+    df['fi'] = df['Edad'].map(df['Edad'].value_counts())
+    #Se calcula la frecuencia absoluta
     df['Fi'] = df['fi'].cumsum()
+    #Se calcula la frecuencia relativa
     df['ri'] = df['fi'] / df['fi'].sum()
+    #Se calcula la frecuencia relativa acumulada
     df["Ri"] = df["ri"].cumsum()
+    #Se calcula el porcentaje de la frecuencia relativa
     df["pi%"] = df["ri"] * 100
+    #Se calcula el porcentaje acumulado
     df["Pi%"] = df["pi%"].cumsum()
 
+    #Devuelve el DataFrame
     return df
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+#importación de la librería pandas
 import pandas as pd
+
+#importación de la función "analisis_estadístico"
 from datos import analisis_estadistico
 
+#Realicé una lista con las edades
 edades = [19, 29, 19, 22, 23, 19, 30, 19, 19, 19, 20, 20, 20, 18, 22, 19, 34, 34, 21, 21, 22, 28, 29, 19, 20, 19, 25, 28, 21, 22]
 
+#Llama a la función "analisis_estadistico" con la lista de las edades
 resultado = analisis_estadistico(edades)
 
+#Se imprime el resultado
 print(resultado)
